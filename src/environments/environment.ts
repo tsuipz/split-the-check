a47e96beee7e7abaa29bda41b9cef0c127269423
Patrick Tsui <patrick.tsui@rokt.com>
2024-06-30 10:59:15 -0400

    adding new replacement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  production: true, // or false depending on the environment
+  production: true,
   firebase: {
-    apiKey: '${FIREBASE_API_KEY}',
-    authDomain: '${FIREBASE_AUTH_DOMAIN}',
-    projectId: '${FIREBASE_PROJECT_ID}',
-    storageBucket: '${FIREBASE_STORAGE_BUCKET}',
-    messagingSenderId: '${FIREBASE_MESSAGING_SENDER_ID}',
-    appId: '${FIREBASE_APP_ID}',
-    measurementId: '${FIREBASE_MEASUREMENT_ID}',
+    apiKey: '{{FIREBASE_API_KEY}}',
+    authDomain: '{{FIREBASE_AUTH_DOMAIN}}',
+    projectId: '{{FIREBASE_PROJECT_ID}}',
+    storageBucket: '{{FIREBASE_STORAGE_BUCKET}}',
+    messagingSenderId: '{{FIREBASE_MESSAGING_SENDER_ID}}',
+    appId: '{{FIREBASE_APP_ID}}',
+    measurementId: '{{FIREBASE_MEASUREMENT_ID}}',
   },
 };
