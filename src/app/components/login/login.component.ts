fd0befcb9e33245de58130dde6072e28600049f7
Patrick Tsui <113466039+patrokt@users.noreply.github.com>
2024-06-30 13:30:36 -0400

    PT-4: add rxjs (#5)
    * PT-4: add rxjs
    
    * add skip on console for now
    
    * update console

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
