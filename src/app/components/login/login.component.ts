f0178f0f2b0982526d628402b563b0cb586c963a
Patrick Tsui <113466039+patrokt@users.noreply.github.com>
2024-06-30 13:09:44 -0400

    PT: add material (#4)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
