8756a7d2eba64b1cb21d50fa90f93aafdb2d20fb
Patrick Tsui <patrick.tsui@rokt.com>
2024-06-30 11:13:25 -0400

    clean up merge file

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authServiceMock: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
+      imports: [LoginComponent],
+      providers: [{ provide: AuthService, useValue: authServiceMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
