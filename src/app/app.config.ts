45882a28291dbc91c6abbbde4355d3e92ba781f9
Patrick Tsui <patrick.tsui@rokt.com>
2024-06-30 13:07:58 -0400

    PT: add material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,13 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
+    provideAuth(() => getAuth()), provideAnimationsAsync(),
   ],
 };
