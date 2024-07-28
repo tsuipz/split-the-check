6841ccd624259ab9756c1d785072a4be59b07d3e
Patrick Tsui <patrick.tsui@rokt.com>
2024-07-28 16:43:32 -0400

    add signals

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const ngrx = require('@ngrx/eslint-plugin/v9');
 
 module.exports = tseslint.config(
   {
@@ -11,6 +12,7 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      ...ngrx.configs.all,
     ],
     processor: angular.processInlineTemplates,
     rules: {
