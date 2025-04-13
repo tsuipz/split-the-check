838a6a54b38d3ab6fcc0c2b81870124e068b701b
tsuipz <tsuipz@gmail.com>
2025-04-12 22:57:37 -0400

    30: load group session

--- src/app/modules/groups/pages/group/group.component.ts
+++ src/app/modules/groups/pages/group/group.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectGroupId } from '@app/core/stores/router/router.selectors';
+import { take } from 'rxjs';
+import { GroupsActions } from '@app/core/stores/groups';
+import { Router } from '@angular/router';
 @Component({
-    selector: 'app-group',
-    imports: [],
-    templateUrl: './group.component.html',
-    styleUrl: './group.component.scss'
+  selector: 'app-group',
+  imports: [],
+  templateUrl: './group.component.html',
+  styleUrl: './group.component.scss',
 })
-export class GroupComponent {
+export class GroupComponent implements OnInit {
+  public groupId$ = this.store.select(selectGroupId);
 
+  constructor(private store: Store, private router: Router) {}
+
+  public ngOnInit(): void {
+    this.groupId$.pipe(take(1)).subscribe((groupId) => {
+      if (groupId) {
+        this.store.dispatch(GroupsActions.loadGroup({ groupId }));
+      } else {
+        this.router.navigate(['/groups']);
+      }
+    });
+  }
 }
