e193a472f4b90afee6380a42e882867db19de9cd
Patrick Tsui <tsuipz@gmail.com>
2025-04-12 21:16:27 -0400

    update: swap to interface (#28)

--- src/app/core/models/interfaces/group.interface.ts
+++ src/app/core/models/interfaces/group.interface.ts
@@ -1,6 +1,7 @@
 export interface Group {
-  id: number;
-  groupName: string;
-  members: string[];
-  amountSpent: number;
+  id: string; // Unique identifier for the group (Firestore document ID)
+  name: string; // Name of the group
+  members: string[]; // Array of user IDs who are members of the group
+  totalSpent: number; // Total amount spent by the group
+  adminOwners: string[]; // Array of user IDs who are designated as admin owners
 }
