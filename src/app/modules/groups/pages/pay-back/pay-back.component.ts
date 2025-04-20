97bde005f43fd70c24d39a50e2bbc977a6acad6d
tsuipz <tsuipz@gmail.com>
2025-04-20 17:38:42 -0400

    feat: add order for members and payments

--- src/app/modules/groups/pages/pay-back/pay-back.component.ts
+++ src/app/modules/groups/pages/pay-back/pay-back.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Timestamp } from '@angular/fire/firestore';
 import { GroupWithMembers } from '@app/core/stores/groups/groups.selectors';
 import { AuthActions } from '@app/core/stores/auth';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { GroupsActions, GroupsSelectors } from '@app/core/stores/groups';
 import { selectGroupWithMembersFromState } from '@app/core/stores/groups/groups.selectors';
 import { selectGroupId } from '@app/core/stores/router/router.selectors';
 import { Store } from '@ngrx/store';
 import { of } from 'rxjs';
-import { switchMap, take, filter } from 'rxjs/operators';
+import { switchMap, take, filter, map } from 'rxjs/operators';
+import { PaymentsSelectors } from '@app/core/stores/payments';
+import { Payment, User } from '@app/core/models/interfaces';
+import { CommonModule } from '@angular/common';
+
+interface MemberWithPayments {
+  user: User;
+  payments: Payment[];
+}
 
 @Component({
   selector: 'app-pay-back',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pay-back.component.html',
   styleUrl: './pay-back.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PayBackComponent implements OnInit {
   private groupId$ = this.store.select(selectGroupId);
@@ -22,6 +31,14 @@ export class PayBackComponent implements OnInit {
     filter((groupId): groupId is string => groupId !== undefined),
     switchMap((groupId) =>
       this.store.select(GroupsSelectors.selectGroupById(groupId)),
+    ),
+  );
+  public payments$ = this.groupId$.pipe(
+    filter((groupId): groupId is string => groupId !== undefined),
+    switchMap((groupId) =>
+      this.store.select(
+        PaymentsSelectors.selectPaymentsByGroupIdAndUserId(groupId),
+      ),
     ),
   );
   public groupWithMembers$: Observable<GroupWithMembers> = this.group$.pipe(
@@ -42,6 +59,48 @@ export class PayBackComponent implements OnInit {
         AuthActions.loadUsersByIds({ userIds: group.members }),
       );
       return this.store.select(selectGroupWithMembersFromState(group));
+    }),
+  );
+  public members$ = this.groupWithMembers$.pipe(
+    map((group) => {
+      const userMap = new Map<string, User>();
+      group.members.forEach((member) => {
+        userMap.set(member.id, member);
+      });
+      return userMap;
+    }),
+  );
+  public membersWithPayments$ = combineLatest([
+    this.members$,
+    this.payments$,
+  ]).pipe(
+    map(([members, payments]) => {
+      const membersWithPayments: MemberWithPayments[] = [];
+
+      // For each payment, add the payment to the member's payments
+      for (const key of Array.from(members.keys())) {
+        const user = members.get(key);
+        if (!user) {
+          continue;
+        }
+
+        const filteredPayments = payments.filter((payment) => {
+          const isPayer = payment.paidBy.some(
+            (payer) => payer.memberId === key,
+          );
+          const isPayee = payment.splits.some(
+            (split) => split.memberId === key,
+          );
+          return isPayer || isPayee;
+        });
+
+        membersWithPayments.push({
+          user,
+          payments: filteredPayments,
+        });
+      }
+
+      return membersWithPayments;
     }),
   );
 
