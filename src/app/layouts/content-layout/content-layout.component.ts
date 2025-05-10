ddb98326d9fa354d2e9c47d37d483a1e8186a95b
tsuipz <tsuipz@gmail.com>
2025-05-10 01:41:59 -0400

    52: init mobile help

--- src/app/layouts/content-layout/content-layout.component.ts
+++ src/app/layouts/content-layout/content-layout.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-    imports: [
-        FormsModule,
-        RouterOutlet,
-        MatSidenavModule,
-        MatCheckboxModule,
-        SidebarComponent,
-    ],
-    selector: 'app-content-layout',
-    templateUrl: './content-layout.component.html',
-    styleUrls: ['./content-layout.component.scss']
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatCheckboxModule,
+    SidebarComponent,
+    MatIconModule,
+  ],
+  selector: 'app-content-layout',
+  templateUrl: './content-layout.component.html',
+  styleUrls: ['./content-layout.component.scss'],
 })
-export class ContentLayoutComponent {}
+export class ContentLayoutComponent {
+  isMobile = false;
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isMobile = result.matches;
+      });
+  }
+}
